366b00261b4be3e0403808c4ec6c9d113fd9208d
Jim Scott <jscott7880@gmail.com>
2022-03-05 18:21:24 -0800

    ready for submission

---
+++ unit-test-assignment/src/test/java/TestDemoTest.java
@@ -0,0 +1,53 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+class TestDemoTest {
+	
+	private TestDemo testDemo;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		testDemo = new TestDemo();
+	}
+
+	@ParameterizedTest
+	@MethodSource("TestDemoTest#argumentsForAddPositive")
+	void assertThatTwoPositiveNumbersAreAddedCorrectly(int a, int b, int expected, Boolean expectException) {
+		if(!expectException) {
+			assertThat(testDemo.addPositive(a, b)).isEqualTo(expected);
+		} else {assertThatThrownBy(() ->
+				testDemo.addPositive(a, b))
+					.isInstanceOf(IllegalArgumentException.class);
+		}
+		
+		
+	}
+	static Stream<Arguments> argumentsForAddPositive() {
+		return Stream.of(
+			arguments(2, 4, 6, false),
+			arguments(7, 3, 10, false),
+			arguments(0, 1, 1, false),
+			arguments(24, 45, 69, false)
+		);
+	}
+	@Test
+	void assertThatNumbersSquaredIsCorrect() {
+		TestDemo mockDemo = spy(testDemo);
+		doReturn(5).when(mockDemo).getRandomInt();
+		
+		int fiveSquared = mockDemo.randomNumberSquared();
+		assertThat(fiveSquared).isEqualTo(25);
+	}
+}
